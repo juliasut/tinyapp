cab6bda2eb263d52b7ec738ee486ba810aa6951b
juliasut <juliasutygina@gmail.com>
2021-07-08 07:56:27 -0600

    Clean up the comments

--- express_server.js
+++ express_server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
 // The body-parser library will convert the request body from a Buffer into string that we can read.
 // It will then add the data to the req(request) object under the key body.
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
-
+app.use(cookieParser());
 // set embedded js template to view engine
 app.set('view engine', 'ejs');
 
@@ -19,36 +20,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-// app.get('/', (req, res) => {
-//   res.send('Hello!');
-// });
+app.get('/login', (req, res) => {
+  res.redirect('/urls');
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get('/hello', (req, res) => {
-//   res.send('<html><body>Hello <b>World</b></body></html>\n')
-// });
-
-// we pass templateVars obj to the template 'urls_index' in views for ejs to render
-// ejs looks for views folder by default. No need for file extention, just name
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-// routes should be ordered from most specific to least specific
-// res.render() is a route handler to pass URL data to our template
 app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
-// add another page to display a single URL and its shortened form.
-// The end point for it will be in the format /urls/:shortURL.
-// The : in front of id indicates that id is a route parameter.
-// req.params buitl in express only for URL paramameters
-// The value in this part of the url will be available in the req.params object.
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
@@ -58,6 +46,11 @@ app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
+
+app.post('/login', (req, res) => {
+  res.cookie('userName', req.body.username);
+  res.redirect('/urls');
+})
 
 app.post('/urls/:shortURL/delete', (req,res) => {
   const shortURL = req.params.shortURL;
@@ -71,8 +64,6 @@ app.post('urls/:shortURL', (req, res) => {
   res.redirect('/urls');
 });
 
-// POST request has a body, while a GET request does not. To read the buffer = we need a parser express.urlencoded
-// will convert body into a string and add key body to req object
 app.post('/urls', (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
